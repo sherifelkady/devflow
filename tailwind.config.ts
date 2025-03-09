25b816d2218a6e2f45869f943b3bea4e69d3a290
Sherif Elsayed <sherif.elsayed@marketing-home.com>
2025-03-09 13:55:20 +0200

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,29 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          100: "#FF7000",
+          500: "#FFF1E6",
+        },
+        dark: {
+          100: "#000000",
+          200: "#0F1117",
+          300: "#151821",
+          400: "#212734",
+          500: "#3F4354",
+        },
+        light: {
+          900: "#FFFFFF",
+          850: "#FDFDFD",
+          800: "#F4F6F8",
+          700: "#DCE3F1",
+          500: "#7B8EC8",
+          400: "#858EAD",
+        },
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        "space-grotesk": ["var(--font-space-grotesk)"],
       },
     },
   },
