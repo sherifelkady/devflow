ab0847268641f288a817270cd61a9a21717c127d
Sherif Elsayed <sherif.elsayed@marketing-home.com>
2025-03-16 13:55:28 +0200

    git hub auth configration

---
+++ app/(root)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="flex flex-col gap-4 items-center"></div>;
+}
---
+++ components/forms/SocialAuthForm.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Button } from "../ui/button";
+import Image from "next/image";
+import { useSonner } from "sonner";
+
+export default function SocialAuthForm() {
+  const buttonClass =
+    "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
+  // ======================================== Handlers ========================================
+  const handleSignIn = async (provider: "github" | "google") => {
+    //   try{}
+    //   catch(error){
+    //     console.log(error);
+    // }
+  };
+  // ======================================== End Handlers ========================================
+  return (
+    <div className="mt-10 flex flex-wrap gap-2.5">
+      <Button className={buttonClass}>
+        <Image
+          src="/icons/github.svg"
+          alt="Github Logo"
+          width={20}
+          height={20}
+          className="invert-colors mr-2.5 object-contain"
+        />
+        <span>Log in with GitHub</span>
+      </Button>
+      <Button className={buttonClass}>
+        <Image
+          src="/icons/google.svg"
+          alt="Google Logo"
+          width={20}
+          height={20}
+          className="mr-2.5 object-contain"
+        />
+        <span>Log in with Google</span>
+      </Button>
+    </div>
+  );
+}
